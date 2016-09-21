ada5321654af64d835f92d511e8858377d5f7468
RezzedUp <RezzedUp@users.noreply.github.com>
2016-09-20 23:43:28 -0500

    Update commands and add README.md

--- src/main/java/com/rezzedup/gob/command/Command.java
+++ src/main/java/com/rezzedup/gob/command/Command.java
@@ -7,16 +7,27 @@ public abstract class Command
 {
     protected final String[] aliases;
     protected final IDiscordClient client;
+    protected String descrption = "";
     
-    public Command(IDiscordClient client, String[] aliases) 
+    public Command(IDiscordClient client, String[] aliases)
     {
         this.client = client;
         this.aliases = aliases;
     }
     
-    public String[] getAliases()
+    public final String[] getAliases()
     {
         return aliases;
+    }
+    
+    protected final void setDescrption(String descrption)
+    {
+        this.descrption = descrption;
+    }
+    
+    public final String getDescrption()
+    {
+        return descrption;
     }
     
     public abstract void execute(String[] args, IMessage message);
--- src/main/java/com/rezzedup/gob/command/usable/HelpCommand.java
+++ src/main/java/com/rezzedup/gob/command/usable/HelpCommand.java
@@ -2,9 +2,14 @@ package com.rezzedup.gob.command.usable;
 
 import com.rezzedup.gob.command.Command;
 import com.rezzedup.gob.command.CommandParser;
+
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IMessage;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class HelpCommand extends Command
 {
@@ -13,6 +18,8 @@ public class HelpCommand extends Command
     public HelpCommand(IDiscordClient client, CommandParser parser)
     {                                        // :question:
         super(client, new String[]{"help", "?", "\u2753"});
+        setDescrption("Displays all usable commands.");
+        
         this.parser = parser;
     }
     
@@ -20,10 +27,39 @@ public class HelpCommand extends Command
     public void execute(String[] args, IMessage message)
     {
         IChannel channel = message.getChannel();
+        List<String> msg = new ArrayList<>();
+        
+        msg.add("Here's what I can do... \uD83D\uDC7A");
+        msg.add(" - - - ");
+        
+        for (Command command : parser.getRegisteredCommands())
+        {
+            String[] aliases = command.getAliases();
+            String entry = " __" + aliases[0] + "__ - ";
+            
+            if (command.getDescrption().isEmpty())
+            {
+                entry += "`No description.`";
+            }
+            else 
+            {
+                entry += "`" + command.getDescrption() + "`";
+            }
+            
+            if (aliases.length >= 2)
+            {
+                entry += " **Aliases:** ";
+                entry += String.join(", ", Arrays.copyOfRange(aliases, 1, aliases.length));
+            }
+            
+            msg.add(entry);
+        }
+        
+        msg.add(" - - -");
         
         try
         {
-            channel.sendMessage("I can't do anything yet...");
+            channel.sendMessage(String.join("\n", msg));
         }
         catch (Exception e)
         {
